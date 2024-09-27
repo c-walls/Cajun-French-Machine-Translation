b41af6e825751e4e34a3e12ce6f2cf1db63f786c
C_Walls <calebmarcuswalls@gmail.com>
2024-09-27 02:42:59 -0500

    Add text splitting and cleaning

--- text-scraping.py
+++ text-scraping.py
@@ -1,6 +1,8 @@
+import re
 import os
 import sys
 import pytesseract
+from tqdm import tqdm
 from PIL import Image
 
 # Configure Tesseract path
@@ -9,7 +11,7 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 def ocr_image(image_path, language):
     try:
         image = Image.open(image_path)
-        return pytesseract.image_to_string(image, lang=language)
+        return "<><page-start><>" + pytesseract.image_to_string(image, lang=language) + "<><page-end><>"
     except Exception as e:
         print(f"Error processing {image_path}: {e}")
         return ""
@@ -18,7 +20,7 @@ def process_images(input_folder):
     english_text = []
     cajun_french_text = []
     files = sorted(os.listdir(input_folder))
-    for i, image in enumerate(files):
+    for i, image in enumerate(tqdm(files, desc="Processing images")):
         image_path = os.path.join(input_folder, image)
         if image.lower().endswith((".png", ".jpg", ".jpeg")):
             if i % 2 == 0:
@@ -28,6 +30,17 @@ def process_images(input_folder):
     
     return english_text, cajun_french_text
 
+def sentence_splitter(text):
+    text = re.sub(r"<><page-start><>.*?\n", "", text)
+    text = re.sub(r"\d{0,3}\s*<><page-end><>", "", text)
+    text = text.replace('\n\n', '||')
+    text = re.sub(r'\|\|.{0,2}$', '', text)
+    text = text.lstrip('\n')
+    text = text.replace('\n', ' ')
+    text = re.sub(r'\s+([».!?])', r'\1', text)
+    text = re.sub(r'«\s+', '«', text)
+    text = re.split(r"(?<=[».!?])\s*(?=[—«A-ZÇÀÉ])|(?<=[».!?])\|\|(?=[—«A-ZÇÀÉ])", text)
+    return text
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -40,3 +53,11 @@ if __name__ == "__main__":
     input_folder = sys.argv[1]
     english_text, cajun_french_text = process_images(input_folder)
 
+    english_text = [sentence_splitter(text) for text in english_text]
+    cajun_french_text = [sentence_splitter(text) for text in cajun_french_text]
+
+    for page in english_text:
+        print("{")
+        for sentence in page:
+            print(f"    [{sentence}]")
+        print("}")
